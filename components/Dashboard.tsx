19341af76218c1df52e3fe0146e84f567f55aaad
Aurora Scharff <aurora.sofie@gmail.com>
2025-04-02 10:04:03 +0200

    Update

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
 import { getCurrentAccount } from '@/data/services/account';
 import Skeleton from './ui/Skeleton';
+
+function DashboardItem({ children, isPro }: { children: React.ReactNode; isPro?: boolean }) {
+  return (
+    <li>
+      <Link
+        href="#"
+        className="group flex w-full items-center justify-between gap-2 rounded bg-gray-light p-4 dark:bg-gray-dark"
+      >
+        <span className="group-hover:underline">{children}</span>
+        <span>{isPro && '☆'}</span>
+      </Link>
+    </li>
+  );
+}
 
 export default async function Dashboard() {
   const account = await getCurrentAccount();
@@ -8,18 +23,18 @@ export default async function Dashboard() {
 
   return (
     <div className="flex flex-col gap-4 px-4">
-      <div>
+      <div className="flex flex-col gap-2">
         <h1 className="text-3xl"> Welcome, {account?.name}!</h1>
-        <h2 className="text-lg">What are you working on today?</h2>
+        <h2 className="text-sm">What are you working on today?</h2>
       </div>
-      <ul className="grid grid-cols-2 gap-2 *:rounded *:bg-gray-light *:p-2 dark:bg-gray-dark">
-        <li>Feature 1</li>
-        <li>Feature 2</li>
-        <li>Feature 3</li>
+      <ul className="flex grid-cols-2 flex-col gap-4 sm:grid dark:bg-gray-dark">
+        <DashboardItem>Feature 1</DashboardItem>
+        <DashboardItem>Feature 2</DashboardItem>
+        <DashboardItem>Feature 3</DashboardItem>
         {isPro && (
           <>
-            <li>Pro Feature 1 ☆</li>
-            <li>Pro Feature 2 ☆</li>
+            <DashboardItem isPro>Pro Feature 1 </DashboardItem>
+            <DashboardItem isPro>Pro Feature 2</DashboardItem>
           </>
         )}
       </ul>
