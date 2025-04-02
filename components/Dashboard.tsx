76db7f24ca4c9851071e81fec44d69ed42ef5e5c
Aurora Scharff <aurora.sofie@gmail.com>
2025-04-02 09:12:43 +0200

    Update

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { getCurrentAccount } from '@/data/services/account';
+import Skeleton from './ui/Skeleton';
 
 export default async function Dashboard() {
   const account = await getCurrentAccount();
   const isPro = account?.plan === 'pro';
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 px-4">
       <div>
         <h1 className="text-3xl"> Welcome, {account?.name}!</h1>
         <h2 className="text-lg">What are you working on today?</h2>
       </div>
-      <ul className="flex flex-col gap-2 bg-gray-light p-4 dark:bg-gray-dark">
+      <ul className="grid grid-cols-2 gap-2 *:rounded *:bg-gray-light *:p-2 dark:bg-gray-dark">
         <li>Feature 1</li>
         <li>Feature 2</li>
         <li>Feature 3</li>
@@ -22,6 +23,14 @@ export default async function Dashboard() {
           </>
         )}
       </ul>
+    </div>
+  );
+}
+
+export function DashboardSkeleton() {
+  return (
+    <div className="px-4">
+      <Skeleton />
     </div>
   );
 }
