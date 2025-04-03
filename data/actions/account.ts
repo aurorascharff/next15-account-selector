7e48e18732efd0a065e25197357a6c3c27263be3
Aurora Scharff <aurora.sofie@gmail.com>
2025-04-03 14:49:53 +0200

    Update

--- data/actions/account.ts
+++ data/actions/account.ts
@@ -6,6 +6,11 @@ import { slow } from '@/utils/slow';
 export async function setCurrentAccount(accountId: string) {
   await slow();
 
+  // Simulate failure for a specific account ID
+  if (accountId === 'd71ab200-18ed-4384-a4a7-a907bf169c9f') {
+    return;
+  }
+
   (await cookies()).set('selectedAccountId', accountId);
 }
 
