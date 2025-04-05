85c2bb5902645f7cab5622d230a45b3c5fd9dc13
Aurora Scharff <aurora.sofie@gmail.com>
2025-04-05 18:15:05 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -11,24 +11,18 @@ export default async function RootPage() {
 
   return (
     <div className="flex flex-col gap-8">
-      <div className="px-4">
-        <Suspense fallback={<AccountSelectorSkeleton />}>
-          <AccountSelector accountsPromise={accounts} currentAccountPromise={currentAccount} />
-        </Suspense>
-      </div>
+      <Suspense fallback={<AccountSelectorSkeleton />}>
+        <AccountSelector accountsPromise={accounts} currentAccountPromise={currentAccount} />
+      </Suspense>
       <Divider theme="primary" />
-      <div className="flex flex-col gap-4 px-4">
-        <div className="mt-4 flex flex-col gap-2">
-          <Suspense fallback={<WelcomeMessageSkeleton />}>
-            <WelcomeMessage />
-          </Suspense>
-        </div>
-        <div className="@container flex flex-col gap-4">
-          <h2 className="dark:text-gray text-base text-gray-500">What are you working on?</h2>
-          <Suspense fallback={<DashboardSkeleton />}>
-            <Dashboard />
-          </Suspense>
-        </div>
+      <div className="mt-2 flex flex-col gap-4">
+        <Suspense fallback={<WelcomeMessageSkeleton />}>
+          <WelcomeMessage />
+        </Suspense>
+        <h2 className="dark:text-gray mt-4 text-base text-gray-500">What are you working on?</h2>
+        <Suspense fallback={<DashboardSkeleton />}>
+          <Dashboard />
+        </Suspense>
       </div>
     </div>
   );
