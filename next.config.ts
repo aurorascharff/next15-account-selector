f756c2b924dd341892b0f4413d25211e37076995
Aurora Scharff <aurora.sofie@gmail.com>
2025-04-05 18:02:53 +0200

    Update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     authInterrupts: true,
+    inlineCss: true,
     reactCompiler: true,
+    staleTimes: {
+      dynamic: 30,
+    },
   },
 };
 
