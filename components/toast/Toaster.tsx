69612674dcb7a0360572b8c562cfe59007f3ee19
Aurora Scharff <aurora.sofie@gmail.com>
2025-04-13 13:23:42 +0200

    Update

--- components/toast/Toaster.tsx
+++ components/toast/Toaster.tsx
@@ -1,9 +1,17 @@
 import { cookies } from 'next/headers';
 import { Toasts } from './Toasts';
 
+type ToastType = 'success' | 'error' | 'info';
+
+export type Toast = {
+  id: string;
+  message: string;
+  type: ToastType;
+};
+
 export async function Toaster() {
   const cookieStore = await cookies();
-  const toasts = cookieStore
+  const toasts: Toast[] = cookieStore
     .getAll()
     .filter(cookie => {
       return cookie.name.startsWith('toast-') && cookie.value;
@@ -12,7 +20,7 @@ export async function Toaster() {
       return {
         id: cookie.name,
         message: cookie.value,
-        type: cookie.name.split('-')[1] as 'success' | 'error' | 'info',
+        type: cookie.name.split('-')[1] as ToastType,
       };
     });
 
