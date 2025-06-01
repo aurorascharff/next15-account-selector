53fd9c9937de7aba3975022d8d506343a39cd1d9
Aurora Scharff <aurora.sofie@gmail.com>
2025-06-01 17:03:00 +0200

    Add server fn to starter

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -1,5 +1,6 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { getAccount } from '@/data/services/account';
 import { slow } from '@/utils/slow';
 import type { NextRequest } from 'next/server';
 
@@ -7,9 +8,9 @@ export async function POST(request: NextRequest) {
   await slow();
 
   const accountId = await request.json();
+  const account = await getAccount(accountId);
 
-  // Simulate failure for a specific account ID
-  if (accountId === 'd71ab200-18ed-4384-a4a7-a907bf169c9f') {
+  if (account.inactive) {
     return NextResponse.json({ error: 'This account is currently inactive.' }, { status: 400 });
   }
 
