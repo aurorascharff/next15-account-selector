84379b4418f9c4c75478c3706bdf9533a73edf5f
Aurora Scharff <aurora.sofie@gmail.com>
2025-06-01 21:27:33 +0200

    Add viewtransition to account circle

--- components/AccountCircle.tsx
+++ components/AccountCircle.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { unstable_ViewTransition as ViewTransition } from 'react';
 import { getCurrentAccount } from '@/data/services/account';
 
 export default async function AccountCircle() {
@@ -8,13 +8,15 @@ export default async function AccountCircle() {
   return (
     <div className="flex flex-col items-center gap-4 md:gap-10">
       <div className="bg-section size-20 rounded-full md:size-30 lg:size-[150px]">
-        <Image
-          src={'/' + currentAccount?.id + '.jpg'}
-          alt="Profile Picture"
-          width={150}
-          height={150}
-          className="rounded-full"
-        />
+        <ViewTransition>
+          <Image
+            src={'/' + currentAccount?.id + '.jpg'}
+            alt="Profile Picture"
+            width={150}
+            height={150}
+            className="rounded-full"
+          />
+        </ViewTransition>
       </div>
     </div>
   );
